eb237a33459051ea4b118733bd4995a36c29be3e
Matthew Bierman <BiermanM@users.noreply.github.com>
2017-01-01 21:24:47 -0800

    Fixed spacing for comments

--- Project 2/ComplexNumber.h
+++ Project 2/ComplexNumber.h
@@ -17,17 +17,17 @@ using namespace std;
 class ComplexNumber
 {
 private:
-    double realNumberComponent;                             // real number component of the complex number
-    double imaginaryNumberComponent;                        // imaginary number component of the complex number
+    double realNumberComponent;                                 // real number component of the complex number
+    double imaginaryNumberComponent;                            // imaginary number component of the complex number
 public:
     ComplexNumber();                                            // default constructor
     ComplexNumber(double, double);                              // overloaded constructor
     ComplexNumber(ComplexNumber&);                              // copy constructor
     double getRealNumberComponent() const;                      // real number component accessor
     double getImaginaryNumberComponent() const;                 // imaginary number component accessor
-	void setRealNumberComponent(double);                        // real number component mutator
-	void setImaginaryNumberComponent(double);                   // imaginary number component mutator
-	ComplexNumber operator+(const ComplexNumber&) const;        // overloaded addition operator
+    void setRealNumberComponent(double);                        // real number component mutator
+    void setImaginaryNumberComponent(double);                   // imaginary number component mutator
+    ComplexNumber operator+(const ComplexNumber&) const;        // overloaded addition operator
     ComplexNumber operator-(const ComplexNumber&) const;        // overloaded subtraction operator
     ComplexNumber operator*(const ComplexNumber&) const;        // overloaded multiplication operator
     ComplexNumber operator/(const ComplexNumber&) const;        // overloaded division operator
